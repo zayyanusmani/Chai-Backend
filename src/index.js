4a05d6eec97ca8e7e217399b4d594305cd77ace5
zayyanusmani <zayyanusmani6@gmail.com>
2024-10-12 20:43:05 +0500

    no updte

---
+++ src/index.js
@@ -0,0 +1,54 @@
+require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+
+
+import mongoose from "mongoose";
+import { DB_NAME } from  "./constants";
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express()
+
+( async () => {
+    try{
+        await mongoose.connect(`${process.env.
+            MONGODB_URI}/${DB_NAME}`)
+            app.on("error", () => {
+                console.log("ERRR: ", error);
+                throw error
+            })
+
+            app.listen(process.env.PORT, () => {
+                console.log(`App is listening to PORT ${process.env.PORT}`);
+            })
+    } catch(error){
+        console.error("ERROR: ",error)
+        throw err
+    }
+})
+
+*/
